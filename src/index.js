de8ccfa3c0b8329cad81ac42f72c6fac5bb2a516
Pablo Cardona <pabcar03@gmail.com>
2020-07-21 23:28:29 -0500

    Added update and delete endpoint for task and user

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('./db/mongoose');
 
 const User = require('./models/user');
 const Task = require('./models/task');
+const { response } = require('express');
 
 const app = express();
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-//Crear un usuario
+//Crete user
 app.post('/users', async (req, res) => {
     try {
         let user = new User(req.body);
@@ -23,6 +24,7 @@ app.post('/users', async (req, res) => {
     };
 })
 
+//Get all users
 app.get('/users', async (req, res) => {
 
     try {
@@ -33,6 +35,7 @@ app.get('/users', async (req, res) => {
     }
 })
 
+//Get single user
 app.get('/users/:id', async (req, res) => {
 
     const id = req.params.id;
@@ -48,7 +51,46 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-//Crear una nota
+//Update a user
+app.patch('/users/:id', async (req, res) => {
+    let id = req.params.id;
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    let isValidOperation = updates.every((value) => allowedUpdates.includes(value))
+
+    if(!isValidOperation)
+        return res.status(403).send({ error: 'Cannot update property'})
+
+    try {
+
+        let user = await User.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if(!user) 
+            return response.status(404).send();
+
+        res.send(user)
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+//Delete a user
+app.delete('/users/:id', async (req, res) => {
+
+    let id = req.params.id;
+    try {
+        await User.findByIdAndDelete(id);
+        res.send()
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+//Create a task
 app.post('/tasks', async (req, res) => {
 
     try {
@@ -60,7 +102,7 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-//Leer todas las notas
+//Get all tasks
 app.get('/tasks', async (req, res)=>{
     
     try {
@@ -71,8 +113,7 @@ app.get('/tasks', async (req, res)=>{
     };
 } )
 
-//Leer una sola nota
-
+//Get single task
 app.get('/tasks/:id', async (req, res)=> {
     
     const id = req.params.id;
@@ -83,6 +124,45 @@ app.get('/tasks/:id', async (req, res)=> {
         res.send(task)
     } catch(err) {
         res.status(500).send(err)
+    }
+})
+
+//Update a task
+app.patch('/tasks/:id', async (req, res) => {
+    let id = req.params.id;
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed']
+    let isValidOperation = updates.every((value) => allowedUpdates.includes(value))
+
+    if(!isValidOperation)
+        return res.status(403).send({ error: 'Cannot update property'})
+
+    try {
+
+        let task = await Task.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if(!task) 
+            return response.status(404).send();
+
+        res.send(task)
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+//Delete a task
+app.delete('/tasks/:id', async (req, res) => {
+    
+    let id = req.params.id;
+    try {
+        await Task.findByIdAndDelete(id);
+        res.send()
+    } catch (err) {
+        res.status(500).send(err);
     }
 })
 
