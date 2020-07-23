58f7f90a63dc6cd71a37485ea3bf7087c2f4d0cd
Pablo Cardona <pabcar03@gmail.com>
2020-07-23 06:10:02 -0500

    Changed config for migration to linux

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const connectionURL = 'mongodb://127.0.0.1:27017' //Definir la conexión [No usar "localhost"]
 const databaseName = 'task-manager-api' //Definir el nombre de la base de datos
 
+mongoose.set('useFindAndModify', false);
+
 mongoose.connect(connectionURL + "/" + databaseName, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
